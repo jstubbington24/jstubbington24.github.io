9101bc49a647cecb6922637e19bee2c165b4a73e
Jacob Stubbington <jstubbington24@gmail.com>
2019-03-02 21:06:47 -0500

    initial Commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<meta charset="utf-8">
+<head>
+  <title>Website</title>
+
+  <meta name="description" content="">
+  <meta name="author" content="Jacob Stubbington">
+</head>
+<body>
+<h1>test<h1>
+
+</body>
